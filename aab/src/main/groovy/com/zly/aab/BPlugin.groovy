c44defda836a83054a6f0d9caed667e78b67d020
liyin <zhangzhangliyin@gmail.com>
2022-08-25 17:40:52 +0800

    增加 Transform, ClassVisitor, AsmClassVisitorFactory ,build success

--- aab/src/main/groovy/com/zly/aab/BPlugin.groovy
+++ aab/src/main/groovy/com/zly/aab/BPlugin.groovy
@@ -1,5 +1,11 @@
 package com.zly.aab
 
+import com.android.build.api.instrumentation.FramesComputationMode
+import com.android.build.api.instrumentation.InstrumentationScope
+import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.api.variant.Variant
+import com.zly.aabb.BBFactory
+import kotlin.jvm.functions.Function1
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,6 +13,18 @@ class BPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        println("=========================================================================================> BPlugin")
+        println("==successsuccesssuccesssuccesssuccess===success============================> BPlugin")
+
+        //在这里注册 Transform
+        //AppExtension VS AndroidComponentsExtension
+        AndroidComponentsExtension extension = (AndroidComponentsExtension) project.getExtensions().getByType(AndroidComponentsExtension.class);
+        extension.onVariants(extension.selector().all(), new Function1<Variant, Variant>() {
+            @Override
+            public Variant invoke(Variant variant) {
+                variant.getInstrumentation().transformClassesWith(BBFactory.class, InstrumentationScope.PROJECT, none -> null);
+                variant.getInstrumentation().setAsmFramesComputationMode(FramesComputationMode.COMPUTE_FRAMES_FOR_INSTRUMENTED_METHODS);
+                return variant;
+            }
+        });
     }
 }
---
+++ aab/src/main/java/com/zly/aabb/BClassVisitor.java
@@ -0,0 +1,32 @@
+package com.zly.aabb;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.AdviceAdapter;
+
+class BClassVisitor extends ClassVisitor {
+
+    BClassVisitor(ClassVisitor nextVisitor) {
+        super(Opcodes.ASM5, nextVisitor);
+    }
+
+    @Override
+   public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+        MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
+        MethodVisitor newVisitor = new AdviceAdapter(Opcodes.ASM5, visitor, access, name, descriptor) {
+            @Override
+            protected void onMethodEnter() {
+                System.out.println("access$access , name:$name , descriptor:$descriptor ===> onMethodEnter");
+                super.onMethodEnter();
+            }
+
+            @Override
+            protected void onMethodExit(int opcode) {
+                System.out.println("access$access , name:$name , descriptor:$descriptor ===> onMethodExit opcode:$opcode   <===");
+                super.onMethodExit(opcode);
+            }
+        };
+        return newVisitor;
+    }
+}
